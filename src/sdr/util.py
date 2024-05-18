d316bf74ba34e7f8afe2d6f04404d181d2f46bd4
Patrick Eads <peads@users.noreply.github.com>
2024-05-18 03:16:49 +0000

    accidentally a whole file

---
+++ src/sdr/util.py
@@ -0,0 +1,29 @@
+#
+# This file is part of the sdrterm distribution
+# (https://github.com/peads/sdrterm).
+# with code originally part of the demodulator distribution
+# (https://github.com/peads/demodulator).
+# Copyright (c) 2023-2024 Patrick Eads.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+from contextlib import closing
+import socket
+
+
+# taken from https://stackoverflow.com/a/45690594
+def findPort(host='localhost'):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind((host, 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
