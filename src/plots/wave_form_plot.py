187e4b9857f86de84e44cbbb5f2a116759fb94fb
Patrick Eads <peads@users.noreply.github.com>
2024-05-27 18:57:50 +0000

    fixed errant change

--- src/plots/wave_form_plot.py
+++ src/plots/wave_form_plot.py
@@ -31,7 +31,7 @@ class WaveFormPlot(Plot):
         super().__init__(**kwargs)
         self.xdata = None
 
-    def initPlot(self, n=1000):
+    def initPlot(self, n):
         super().initPlot()
         self.fig, self.ax = plt.subplots()
         self.ln, = self.ax.plot(0, 0)
