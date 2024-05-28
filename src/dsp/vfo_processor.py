1df0a28199072d78b64800488d20b4b6ebd1cd0e
Patrick Eads <peads@users.noreply.github.com>
2024-05-28 00:13:30 -0400

    Refactoring for vectorization (#13)
    * init
    
    * refactored vfo processor vectorize the calculation of multi-outputs, and parallelize processing chunks of input data like the regular dsp processor does.

--- src/dsp/vfo_processor.py
+++ src/dsp/vfo_processor.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+import os
 import socket
 from contextlib import closing
 from functools import partial
@@ -55,26 +56,37 @@ class VfoProcessor(DspProcessor):
                  centerFreq: float,
                  omegaOut: int,
                  tunedFreq: int,
-                 vfos: str,
+                 vfos: list[float] | np.ndarray[any, np.real],
                  correctIq: bool,
                  decimation: int,
                  **kwargs):
+        self.shift = None
         if not vfos or len(vfos) < 1:
             raise ValueError("simo mode cannot be used without the vfos option")
         super().__init__(fs=fs,
                          centerFreq=centerFreq,
                          omegaOut=omegaOut,
                          tunedFreq=tunedFreq,
-                         vfos=vfos,
+                         vfos=np.array(vfos),
                          correctIq=correctIq,
                          decimation=decimation, **kwargs)
 
-    def processVfoChunk(self, y, freq) -> np.ndarray[any, np.real] | None:
+    def __shiftFreqs(self, y):
+        if self.shift is None or len(y) != len(self.shift):
+            t = np.arange(len(y))
+            self.shift = np.array([np.exp(-2j * np.pi * (freq / self.fs) * t) for freq in (self.vfos + self.centerFreq)])
+        y = np.broadcast_to(y, (len(self.vfos), len(y)))
+        return y * self.shift
+
+    def processChunk(self, y: list) -> np.ndarray[any, np.real] | None:
         try:
-            y = shiftFreq(y, freq, self.fs)
+            y = deinterleave(y)
+            if self.correctIq is not None:
+                y = self.correctIq.correctIq(y)
+            y = self.__shiftFreqs(y)
             y = signal.decimate(y, self.decimation, ftype='fir')
             y = signal.sosfilt(self.sosIn, y)
-            y = self.demod(y)
+            y = np.array([self.demod(yy) for yy in y])
             return applyFilters(y, self.outputFilters)
         except KeyboardInterrupt:
             return None
@@ -93,18 +105,22 @@ class VfoProcessor(DspProcessor):
                         lt.start()
 
                         eprint(f'\nAccepting connections on port {server.port}\n')
+                        ii = range(os.cpu_count())
+                        data = []
                         while not isDead.value:
-                            y = inReader.recv()
-                            if y is None or len(y) < 1:
+                            for _ in ii:
+                                y = inReader.recv()
+                                if y is None or not len(y):
+                                    break
+                                data.append(y)
+
+                            if data is None or not len(data):
                                 break
-
-                            y = deinterleave(y)
-                            if self.correctIq is not None:
-                                y = self.correctIq.correctIq(y)
-                            y = shiftFreq(y, self.centerFreq, self.fs)
-
-                            results = pool.map_async(partial(self.processVfoChunk, y), self.vfos)
-                            [outWriter.send(r) for r in results.get()]
+                            y = pool.map_async(self.processChunk, data)
+                            for yy in y.get():
+                                for yyy in yy:
+                                    outWriter.send(yyy)
+                            data.clear()
 
                         pool.close()
                         pool.join()
