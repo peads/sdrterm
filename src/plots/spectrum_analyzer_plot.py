3f774249b50ec28dcbcb35847f9bbaf192c6beec
Patrick Eads <peads@users.noreply.github.com>
2024-06-18 02:23:10 +0000

    moved iq correction and added freq offset to qt plot

--- src/plots/spectrum_analyzer_plot.py
+++ src/plots/spectrum_analyzer_plot.py
@@ -20,6 +20,8 @@
 from multiprocessing import Queue, Value
 from uuid import uuid4
 
+from dsp.iq_correction import IQCorrection
+from dsp.util import shiftFreq
 from misc.general_util import deinterleave
 from plots.qt_spectrum_analyzer import AbstractSpectrumAnalyzer
 
@@ -27,22 +29,32 @@ from plots.qt_spectrum_analyzer import AbstractSpectrumAnalyzer
 class SpectrumAnalyzerPlot(AbstractSpectrumAnalyzer):
     uuid = None
 
-    def __init__(self, fs: int, iq: bool = True, buffer: Queue = None, isDead: Value = None, frameRate: int = 0):
-        super().__init__(fs, iq, frameRate=frameRate)
+    def __init__(self, fs: int,
+                 iq: bool = False,
+                 buffer: Queue = None,
+                 isDead: Value = None,
+                 frameRate: int = 0,
+                 offset: int = 0):
+        super().__init__(fs, frameRate=frameRate)
         self.buffer = buffer
         self.isDead = isDead
+        self.offset = offset
         self.uuid = uuid4()
+        self.iqCorrector = IQCorrection(self.fs) if iq else None
 
     def __del__(self):
         self.buffer.close()
         self.buffer.cancel_join_thread()
 
     def receiveData(self):
-        return deinterleave(self.buffer.get())
+        data = deinterleave(self.buffer.get())
+        if self.iqCorrector is not None:
+            data = self.iqCorrector.correctIq(data)
+        return shiftFreq(data, self.offset, self.fs)
 
     @classmethod
-    def processData(cls, isDead, buffer, fs):
-        cls.start(fs, buffer=buffer, isDead=isDead)
+    def processData(cls, isDead, buffer, fs, **kwargs):
+        cls.start(fs, buffer=buffer, isDead=isDead, **kwargs)
 
     def update(self):
         from pyqtgraph.Qt.QtCore import QCoreApplication
