e50f29b972b0f955176e554e34fd4038f94eafb2
Patrick Eads <peads@users.noreply.github.com>
2024-07-13 23:03:24 +0000

    init

--- src/dsp/demodulation.py
+++ src/dsp/demodulation.py
@@ -17,53 +17,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-
-from numpy import angle, ndarray, conj, abs, real, imag, dtype, complex64, complex128, insert, float32, float64
-from scipy import signal
+from numpy import angle, ndarray, conj, abs, real, imag, dtype, complex64, complex128, float32, float64
 from scipy.fft import fft
+from scipy.signal import resample
 
 
-class __FMDemodulator:
-    prevRe = None
-    prevIm = None
-
-    @classmethod
-    def fmDemod(cls, data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
-        re: ndarray[any, dtype[complex64 | complex128]] = data[0::2]
-        im: ndarray[any, dtype[complex64 | complex128]] = data[1::2]
-
-        if cls.prevRe is not None:
-            insert(re, 0, cls.prevRe)
-            cls.prevRe = None
-        if cls.prevIm is not None:
-            insert(im, 0, cls.prevIm)
-            cls.prevIm = None
-
-        if len(re) > len(im):
-            cls.prevRe = re[-1]
-            re = re[:-1]
-        elif len(im) > len(re):
-            cls.prevIm = im[-1]
-            im = im[:-1]
-        re = re * conj(im)
-        return signal.resample(angle(re), len(data))
+def fmDemod(data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
+    re: ndarray[any, dtype[complex64 | complex128]] = data[0::2]
+    im: ndarray[any, dtype[complex64 | complex128]] = data[1::2]
+    re = re * conj(im)
+    return resample(angle(re), len(data))
 
 
-def fmDemod(data: ndarray[any,  dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
-    return __FMDemodulator.fmDemod(data)
-
-
-def amDemod(data: ndarray[any,  dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
+def amDemod(data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
     return abs(data)
 
 
-def realOutput(data: ndarray[any,  dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
+def realOutput(data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
     return real(data)
 
 
-def imagOutput(data: ndarray[any,  dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
+def imagOutput(data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
     return imag(data)
 
 
-def spectrumOutput(data: ndarray[any,  dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
+def spectrumOutput(data: ndarray[any, dtype[complex64 | complex128]]) -> ndarray[any, dtype[float32 | float64]]:
     return abs(fft(data))
