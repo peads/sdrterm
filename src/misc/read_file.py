758ad5e71df13b325a0b509260b7e32d3186a0da
Patrick Eads <peads@users.noreply.github.com>
2024-07-14 19:01:20 +0000

    cleaning up

--- src/misc/read_file.py
+++ src/misc/read_file.py
@@ -18,9 +18,9 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 from multiprocessing import Value, Process, Queue
+from typing import Iterable
 
 from numpy import dtype
-from typing import Iterable
 
 
 def readFile(bitsPerSample: dtype = None,
